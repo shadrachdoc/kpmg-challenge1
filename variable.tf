e52e871b0079c2d9b3ea960ba8eb5623e00bdd6b
shadrach <shadrachdoc@gmail.com>
2022-03-17 07:38:22 +0000

    correction on variable file

--- variable.tf
+++ variable.tf
@@ -2,14 +2,26 @@ variable "vpc"{
     default = "kpmg"
 }
 variable "web-subnet-1"{
-    default = "kpmg"
+    default = "Web-1a"
 }
-variable "vpc"{
-    default = "kpmg"
+variable "web-subnet-2"{
+    default = "Web-2b"
 }
-variable "vpc"{
-    default = "kpmg"
+variable "application-subnet-1"{
+    default = "kpmg-Application-1a"
 }
-variable "vpc"{
-    default = "kpmg"
+variable "application-subnet-2"{
+    default = "kpmg-Application-2b"
+}
+variable "database-subnet-1"{
+    default = "kpmg-Database-1a"
+}
+variable "database-subnet-2"{
+    default = "kpmg-Database-2b"
+}
+variable "igw"{
+    default = "kpmg-igw"
+}
+variable "web-rt"{
+    default = "kpmg-web-route"
 }
