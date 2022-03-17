fbb72f38f271f61d41f220a2748bba8acb308a90
shadrach <shadrachdoc@gmail.com>
2022-03-17 07:29:38 +0000

    3layer app infra creation via IAAC

---
+++ main.tf
@@ -0,0 +1,187 @@
+# Create a VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.vpc
+  }
+}
+
+# Create Web Public Subnet
+resource "aws_subnet" "web-subnet-1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.web-subnet-1
+  }
+}
+
+resource "aws_subnet" "web-subnet-2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.web-subnet-2
+  }
+}
+
+# Create Application Private Subnet
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.11.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Application-1a"
+  }
+}
+
+resource "aws_subnet" "application-subnet-2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.12.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Application-2b"
+  }
+}
+
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.21.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Database-1a"
+  }
+}
+
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.22.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Database-2b"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "Demo IGW"
+  }
+}
+
+# Create Web layber route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "WebRT"
+  }
+}
+
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.web-subnet-1.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.web-subnet-2.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+# Create Web Security Group
+resource "aws_security_group" "web-sg" {
+  name        = "Web-SG"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-SG"
+  }
+}
+
+# Create Web Server Security Group
+resource "aws_security_group" "webserver-sg" {
+  name        = "Webserver-SG"
+  description = "Allow inbound traffic from ALB"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description     = "Allow traffic from web layer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Webserver-SG"
+  }
+}
+
+# Create Database Security Group
+resource "aws_security_group" "database-sg" {
+  name        = "Database-SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description     = "Allow traffic from application layer"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.webserver-sg.id]
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Database-SG"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "vpc"{
+    default = "kpmg"
+}
+variable "web-subnet-1"{
+    default = "kpmg"
+}
+variable "vpc"{
+    default = "kpmg"
+}
+variable "vpc"{
+    default = "kpmg"
+}
+variable "vpc"{
+    default = "kpmg"
+}
